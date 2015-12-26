d4c9d4584f7659a1182bb63a6d971744e94c2eb3
wyingtian <yingtianwang@g.harvard.edu>
2015-12-25 20:49:57 -0500

    add player and enemy element

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+
+var Enemy = function(posX,posY) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = posX;
+    this.y = posY;
 };
 
 // Update the enemy's position, required method for game
@@ -21,15 +24,45 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    //X, Y co-rdinates of the player
+    this.x = 202;
+    this.y = 556;
+    this.person = 'images/char-boy.png';
+};
 
 
+//Draw the player on the canvas.
+Player.prototype.render = function() {
+   ctx.drawImage(Resources.get(this.person),this.x,this.y);
+};
+
+//Update player movements, track score and collisions
+Player.prototype.update = function() {
+};
+
+//Update player movements based on keyboard inputs
+//Player can move up, down, left and right and
+//limit movement within the canvas
+Player.prototype.handleInput = function(key){
+    switch (key){
+        case 'left':
+            this.x = this.x - 100;
+            
+        
+    }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [ new Enemy(0, 60), new Enemy(101, 150), new Enemy(202, 235), new Enemy(0, 321)];
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
@@ -41,6 +74,11 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
+   
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+
+
+
+
